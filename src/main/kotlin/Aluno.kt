fe949f4c15082529683f928593be5275471933a7
Lucas Sales <lucas.gol@uol.com.br>
2021-09-03 18:04:02 -0300

    Criado classe Aluno

---
+++ src/main/kotlin/Aluno.kt
@@ -0,0 +1,42 @@
+import java.lang.Exception
+
+/*Criar um diagrama de classes que modele a classe Aluno. A princípio, um
+aluno possui um nome (String), um sobrenome (String) e um código de
+aluno (Integer).
+*/
+
+/*Criar um construtor para o aluno que tome como parâmetro um nome, um
+sobrenome e um código de aluno.
+*/
+
+class Aluno(
+    var nome: String = "",
+    var sobrenome: String = "",
+    var codigo: Int = 0,
+) {
+
+//Um aluno será igual a outro se seus códigos de aluno forem iguais.
+
+    override fun equals(other: Any?): Boolean {
+        return (other is Aluno && other.codigo == this.codigo)
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+
+}
+
+/*
+ //Preferiria fazer assim para nao ter problema com codigo do aluno
+class Aluno(
+    var nome: String,
+    var sobrenome: String,
+) {
+    var codigo = 0
+    init{
+        codigo++
+    }
+
+}
+*/
---
+++ src/main/kotlin/Run.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var aluno1 = Aluno("Lucas", "Sales",1)
+    var aluno2 = Aluno("Fernando", "Freitas",1)
+
+
+
+            println(aluno1==aluno2)
+
+
+}
