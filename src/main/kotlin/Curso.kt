87bd8fe8dddd08720c993bcd2a6070bbbf75a227
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 15:32:48 -0300

    Implementado novos atruibutos a curso, qtdMaximaAlunos, listaMatriculados, profTitular, profAdjunto

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -2,11 +2,16 @@
 Criar um diagrama de classes que modele a classe Curso. A princípio, um
 curso possui um nome (String) e um código de curso (Integer).
  */
-
+/*
+Além de ter um nome e código de curso, um curso possui um professor titular
+(ProfessorTitular), um professor adjunto (ProfessorAdjunto), uma quantidade
+máxima de alunos (Integer) e uma lista de alunos matriculados.
+*/
 
 class Curso(
     var nome: String = "",
-    var codigo: Int = 0
+    var codigo: Int = 0,
+    var qtdMaximaAlunos: Int = 0,
 ) {
 
     // Bloco init para validar codigo maior que 0
@@ -16,8 +21,14 @@ class Curso(
         } else if (codigo == 0) {
             codigo = (1..1000).random()
         }
+        if (qtdMaximaAlunos < 0) {
+            qtdMaximaAlunos = 0
+        }
     }
 
+    lateinit var listaMatriculados: List<Aluno>
+    lateinit var profTitular: ProfessorTitular
+    lateinit var profAdjunto: ProfessorAdjunto
 
     //Um curso será igual a outro se seus códigos de curso forem iguais.
 
