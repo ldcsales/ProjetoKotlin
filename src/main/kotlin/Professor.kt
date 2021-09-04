4d9657e2246e76ae07829bb2b33d383df75af265
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 14:42:39 -0300

    Alterado Classe Professor, equal e hashcode

--- src/main/kotlin/Professor.kt
+++ src/main/kotlin/Professor.kt
@@ -4,11 +4,21 @@ class Professor(
     var tempoDeCasa: Int = 0,
     var codigo: Int = 0
 ) {
+    // Bloco init para validar codigo maior que 0
     init {
         if (codigo < 0) {
             codigo = (codigo * -1)
         } else if (codigo == 0) {
             codigo = (1..10000).random()
         }
+    }
+
+    //Um professor será igual a outro se seus códigos de professor forem iguais.
+    override fun equals(other: Any?): Boolean {
+        return (other is Professor && other.codigo == this.codigo)
+    }
+
+    override fun hashCode(): Int {
+        return codigo
     }
 }
