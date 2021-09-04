93680db3f86d162bbf59c99dd7421f209e8c1738
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 14:40:19 -0300

    Alterado Classe Curso

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -9,6 +9,15 @@ class Curso(
     var codigo: Int = 0
 ) {
 
+    // Bloco init para validar codigo maior que 0
+    init {
+        if (codigo < 0) {
+            codigo = (codigo * -1)
+        } else if (codigo == 0) {
+            codigo = (1..1000).random()
+        }
+    }
+
     //Um curso será igual a outro se seus códigos de curso forem iguais.
 
     override fun equals(other: Any?): Boolean {
