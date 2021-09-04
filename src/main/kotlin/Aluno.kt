f09fceb2298a4b9bce4cc0e0974f28af0946884c
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 14:39:21 -0300

    Alterado Classe Aluno

--- src/main/kotlin/Aluno.kt
+++ src/main/kotlin/Aluno.kt
@@ -14,6 +14,14 @@ class Aluno(
     var sobrenome: String = "",
     var codigo: Int = 0,
 ) {
+    // Bloco init para validar codigo maior que 0
+    init {
+        if (codigo < 0) {
+            codigo = (codigo * -1)
+        } else if (codigo == 0) {
+            codigo = (1..1000).random()
+        }
+    }
 
 //Um aluno será igual a outro se seus códigos de aluno forem iguais.
 
