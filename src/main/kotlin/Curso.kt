07c69c3c71a90f5bf444a7d5d85963b270f33925
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 13:58:17 -0300

    Curso documentado

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -1,7 +1,15 @@
+/*
+Criar um diagrama de classes que modele a classe Curso. A princípio, um
+curso possui um nome (String) e um código de curso (Integer).
+ */
+
+
 class Curso(
     var nome: String,
     var codigo: Int
 ) {
+
+    //Um curso será igual a outro se seus códigos de curso forem iguais.
 
     override fun equals(other: Any?): Boolean {
         return (other is Curso && other.codigo == this.codigo)
