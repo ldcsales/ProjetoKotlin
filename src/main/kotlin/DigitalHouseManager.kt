349a319d2197b9750755dedeb927a392adf9f157
Lucas Sales <lucas.gol@uol.com.br>
2021-09-04 17:14:34 -0300

    Adicionado fun registrarCurso

--- src/main/kotlin/DigitalHouseManager.kt
+++ src/main/kotlin/DigitalHouseManager.kt
@@ -10,4 +10,20 @@ class DigitalHouseManager {
     lateinit var listaProfessores: MutableList<Professor>
     lateinit var listaCursos: MutableList<Curso>
     lateinit var listaMatriculas: MutableList<Matricula>
+
+    fun registrarCurso(
+        nome: String,
+        codigoCurso: Int,
+        quantidadeMaximaDeAlunos: Int
+    ) {
+        try {
+            var curso = Curso(nome, codigoCurso, quantidadeMaximaDeAlunos)
+            listaCursos.add(curso)
+            println("Curso Registrado")
+        } catch (ex: Exception) {
+            println("Nao foi possivel registrar esse curso! Tente Novamente.")
+        }
+
+
+    }
 }
